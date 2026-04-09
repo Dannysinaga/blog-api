bb55fac3ffdf5d6a57a8e67e324e7dbae703c8d2
Dannysinaga <danny.marulitua@gmail.com>
2026-04-09 11:10:52 +0700

    feat: add login  feature (#1)

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,5 +7,9 @@ export class AuthController {
   register = async (req: Request, res: Response) => {
     const result = await this.authService.register(req.body);
     res.status(200).send(result);
+  }
+    login = async (req: Request, res: Response) => {
+    const result = await this.authService.login(req.body);
+    res.status(200).send(result);
   };
 }

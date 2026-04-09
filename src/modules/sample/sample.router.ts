8e91b8fbb0572f34b93bc9d92d03874e7658bf84
Danny Sinaga <danny.marulitua@gmail.com>
2026-04-09 15:20:33 +0700

    feat: add jwt middleware &rbac

--- src/modules/sample/sample.router.ts
+++ src/modules/sample/sample.router.ts
@@ -1,18 +1,29 @@
 import { Router } from "express";
 import { SampleController } from "./sample.controller.js";
+import { AuthMiddleware } from "../../middlewares/auth.middleware.js";
 
 export class SampleRouter {
   router: Router;
 
-  constructor(private sampleController: SampleController) {
+  constructor(private sampleController: SampleController, private authMiddleware: AuthMiddleware) {
     this.router = Router();
     this.initRoutes();
   }
 
-  private initRoutes = () => {
-    this.router.get("/", this.sampleController.getSamples);
-    this.router.get("/:id", this.sampleController.getSample);
-  };
+private initRoutes = () => {
+    this.router.get(
+        "/", // 1
+        this.authMiddleware.verifyToken(process.env.JWT_SECRET!), // 2
+        this.sampleController.getSamples, // 3
+    );
+
+    this.router.get(
+        "/:id", // 1
+        this.authMiddleware.verifyToken(process.env.JWT_SECRET!), // 2
+        this.authMiddleware.verifyRole(["USER"]), // 3
+        this.sampleController.getSample, // 4
+    );
+};
 
   getRouter = () => {
     return this.router;
